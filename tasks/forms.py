b2f105b39c2a3f17e6d40b1dcc0fd995ed2a54d0
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-05-09 02:26:06 +0430

    few edits in tasks app

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
 
-class taskForm(forms.Form):
+class TaskForm(forms.Form):
     title = forms.CharField(required=True, max_length=200)
+
+    # class EditTaskForm(forms.Form):
+    # Deletechoices = forms.MultipleChoiceField(
+    #         widget  = forms.CheckboxSelectMultiple,
+    #     )
+    #     Donechoices = forms.MultipleChoiceField(
+    #         widget  = forms.CheckboxSelectMultiple,
+    #     )
--- tasks/views.py
+++ tasks/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
 
 from tasks.models import Tasks
-from tasks.forms import taskForm
+
+from tasks.forms import TaskForm
 
 
 def tasks(request, listid):
-    newTaskForm = taskForm()
+    newTaskForm = TaskForm()
+
     if request.method == "POST":
-        newTaskForm = taskForm(request.POST)
+        delChoices = request.POST.getlist('Deletechoices[]')
+        donChoices = request.POST.getlist('Donechoices[]')
+        for i in delChoices:
+            Tasks.objects.filter(id=i).delete()
+        for i in donChoices:
+            Tasks.objects.filter(id=i).update(list_id='4')
+
+        newTaskForm = TaskForm(request.POST)
         if newTaskForm.is_valid():
             newRow = Tasks();
             newRow.title = newTaskForm.cleaned_data['title']
             newRow.list_id = int(listid)
             newRow.save()
         else:
-            newTaskForm = taskForm()
+            newTaskForm = TaskForm()
 
     return render(request, 'tasks.html', {
         'PageTitle': "CeDoTasks - Tasks",
         'task': Tasks.objects.filter(list_id=listid),
-        'newTaskForm': newTaskForm
+        'newTaskForm': newTaskForm,
     })
